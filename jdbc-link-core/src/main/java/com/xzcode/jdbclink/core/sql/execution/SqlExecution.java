aa68e3bbb2301b947aa759a5901476143dcb81ff
xz <379551875@qq.com>
2019-04-02 18:47:13 +0800

    添加sql insert功能

--- jdbc-link-core/src/main/java/com/xzcode/jdbclink/core/sql/execution/SqlExecution.java
+++ jdbc-link-core/src/main/java/com/xzcode/jdbclink/core/sql/execution/SqlExecution.java
@@ -1,7 +1,12 @@
 package com.xzcode.jdbclink.core.sql.execution;
 
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +14,13 @@ import java.util.Map;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+
+import com.xzcode.jdbclink.core.entity.EntityFieldInfo;
+import com.xzcode.jdbclink.core.entity.IEntity;
+import com.xzcode.jdbclink.core.util.ShowSqlUtil;
 
 public class SqlExecution{
 	
@@ -71,6 +82,41 @@ public class SqlExecution{
 		return this.jdbcTemplate.update(sql, args);
 	}
 
+	/**
+	 * 执行insert
+	 * 
+	 * @return
+	 * @author zai
+	 * 2019-04-02 14:16:13
+	 */
+	public int executeInsert() {
+		try {
+
+			ShowSqlUtil.debugSqlAndParams(this.sql, args);
+			
+			PreparedStatementCreator statement = new PreparedStatementCreator() {
+				
+				@Override
+				public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+					PreparedStatement prepareStatement = con.prepareStatement(sql);
+					
+					if (args != null) {
+						for (int i = 1; i <= args.length; i++) {
+							prepareStatement.setObject(i, args[i - 1]);
+						}
+					}
+					
+					return prepareStatement;
+				}
+			};
+			
+			return this.jdbcTemplate.update(statement);
+		
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
 	
 	
 	public <F> List<F> queryFirstColumn(Class<F> f) {
